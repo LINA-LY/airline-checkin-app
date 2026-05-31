7523b508dfdeada374088c3b91967744ecdd2f5e
SariYanouche <sariyanouche@gmail.com>
2026-05-31 23:04:23 +0100

    m

--- app/src/main/java/com/airline/checkin/ui/AppNavGraph.kt
+++ app/src/main/java/com/airline/checkin/ui/AppNavGraph.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
